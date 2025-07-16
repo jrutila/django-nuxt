d58b616a82c0ae83bf3ea1bc83bef89f38b68023
Juho Rutila <juho.rutila@gmail.com>
2025-07-16 13:39:43 +0300

    feat: add static urls

---
+++ django_nuxt/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.urls import path, re_path
+from django.views.static import serve
+
+def NuxtStaticUrls():
+    if settings.DEBUG:
+        from django.views.generic import View
+        from django.http import HttpResponse
+        
+        class NuxtProxyView(View):
+            def get(self, request, *args, **kwargs):
+                from django.shortcuts import redirect
+                try:
+                    return redirect('http://localhost:3000' + request.path)
+                except:
+                    return HttpResponse("Nuxt is not running on localhost:3000", status=503)
+        
+        return [
+            path('_nuxt/<path:path>', NuxtProxyView.as_view(), name='nuxt_proxy'),
+            path('__nuxt_devtools__/<path:path>', NuxtProxyView.as_view(), name='nuxt_devtools_proxy')
+        ]
+
+    return [
+        re_path(r'^_nuxt/(?P<path>.*)$', serve, kwargs={
+            'document_root': 'staticfiles/_nuxt',
+        }),
+    ]
