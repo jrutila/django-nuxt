74960c219a5ee6d5ade9b0ebeffb3fd0b204e786
Juho Rutila <juho.rutila@gmail.com>
2025-07-17 10:47:36 +0300

    feat: use nuxt_django in the example basic

--- example/basic/ui/nuxt.config.ts
+++ example/basic/ui/nuxt.config.ts
@@ -2,5 +2,7 @@
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
+  modules: ['../../../nuxt_django/src/module'],
+  nuxtDjango: {},
   devtools: { enabled: true }
 })
