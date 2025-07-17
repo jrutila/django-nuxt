15066ff6436640499edbcdf12dff1f5042b3f697
Juho Rutila <juho.rutila@gmail.com>
2025-07-17 14:18:37 +0300

    feat: example add nuxt/ui

--- example/basic/ui/app/app.vue
+++ example/basic/ui/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <UApp>
     <NuxtRouteAnnouncer />
     <div>
       Hello {{ user.username }}
@@ -13,14 +13,27 @@
       <button @click="refresh">Refresh</button>
     </div>
     {{ schema }}
-  </div>
+    <UForm :schema="schema" v-model="newTodo">
+      <UFormField v-for="field in fields" :key="field[0]" :name="field[0]" :label="field[1].label" :type="field[1].type">
+        <UInput v-model="newTodo[field[0]]" />
+      </UFormField>
+      <UButton
+        label="Create"
+        color="primary"
+        variant="solid"
+        :loading="creating"
+        type="submit"
+      />
+    </UForm>
+  </UApp>
 </template>
 
 <script setup>
 const user = ref(window.django_nuxt.user)
 
-const djangoNuxt = useDjangoNuxt()
-
 const { data: todos, refresh } = await useDjangoModel('todo')
 const { data: schema } = await useDjangoSchema('todo')
+
+const newTodo = ref({})
+const fields = Object.entries(schema.value || {}).filter(([key, field]) => !key.startsWith("~") && !field.read_only)
 </script>
--- example/basic/ui/nuxt.config.ts
+++ example/basic/ui/nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
-  modules: ['../../../nuxt_django/src/module'],
+  modules: ['../../../nuxt_django/src/module', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
   nuxtDjango: {},
   devtools: { enabled: true },
   runtimeConfig: {
