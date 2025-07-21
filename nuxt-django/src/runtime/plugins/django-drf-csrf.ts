6579cb1e425585df456f1d6ef9e98f5e0f6475a4
Juho Rutila <juho.rutila@gmail.com>
2025-07-21 09:38:02 +0300

    fix: baseURl on GETs

--- nuxt-django/src/runtime/plugins/django-drf-csrf.ts
+++ nuxt-django/src/runtime/plugins/django-drf-csrf.ts
@@ -1,21 +1,19 @@
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
 export default defineNuxtPlugin(() => {
-  console.log('django-drf-csrf plugin')
   globalThis.$fetch = $fetch.create({
     onRequest({ request, options }) {
       const config = useRuntimeConfig()
       const apiPath = config.public.nuxtDjango?.apiPath || '/api/'
-      console.log('onRequest', apiPath, request)
 
       if (!request.startsWith(apiPath)) {
         return
       }
 
-      if (options.method && options.method !== "GET") {
-        const baseURL = config.public.nuxtDjango?.baseURL || '/'
-        options.baseURL = baseURL
+      const baseURL = config.public.nuxtDjango?.baseURL || '/'
+      options.baseURL = baseURL
 
+      if (options.method && options.method !== "GET") {
         const csrftoken = document.cookie.split('; ').find(row => row.startsWith('csrftoken='))?.split('=')[1]
         if (csrftoken) {
           options.headers.set('X-CSRFToken', csrftoken)
