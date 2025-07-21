a10d82f0dcbbc5865b324b6110dfa6fe306aa43e
Juho Rutila <juho.rutila@gmail.com>
2025-07-21 12:55:27 +0300

    fix: manual imports instead of imports dir

--- nuxt-django/src/module.ts
+++ nuxt-django/src/module.ts
@@ -23,23 +23,24 @@ export default defineNuxtModule<ModuleOptions>({
     addPlugin(resolver.resolve('./runtime/plugins/django-drf-csrf'))
     addPlugin(resolver.resolve('./runtime/plugins/django-nuxt'))
     addImports([
-      {
-        from: resolver.resolve('./runtime/composables/useDjangoNuxt'),
-        name: 'useDjangoNuxt',
-        as: 'useDjangoNuxt',
-      },
-      {
-        from: resolver.resolve('./runtime/composables/useDjangoSchema'),
-        name: 'useDjangoSchema',
-        as: 'useDjangoSchema',
-      },
-      {
-        from: resolver.resolve('./runtime/composables/useDjangoModel'),
-        name: 'useDjangoModel',
-        as: 'useDjangoModel',
-      },
-    ])
-    addImportsDir(resolver.resolve('./runtime/utils/'), { prepend: true })
+      'useDjangoNuxt', 'useDjangoSchema', 'useDjangoModel',
+    ].map(name => ({
+      from: resolver.resolve(`./runtime/composables/${name}`),
+      name,
+      as: name,
+    })))
+    addImports([
+      'createDjangoModel', 'updateDjangoModel', 'patchDjangoModel', 'deleteDjangoModel',
+    ].map(name => ({
+      from: resolver.resolve(`./runtime/utils/crud`),
+      name,
+      as: name,
+    })))
+    addImports([{
+      from: resolver.resolve(`./runtime/utils/ui-form-server-errors`),
+      name: 'handDjangoServerErrors',
+      as: 'handDjangoServerErrors',
+    }])
     nuxt.options.runtimeConfig.public.nuxtDjango = (nuxt.options.runtimeConfig.public.nuxtDjango || {}) as NuxtDjangoRuntimeConfig
     nuxt.options.runtimeConfig.public.nuxtDjango.schemaKey = options.schemaKey || '{{ NUXT_DJANGO_SCHEMA_KEY }}'
     nuxt.options.runtimeConfig.public.nuxtDjango.apiPath = options.apiPath || '{{ NUXT_DJANGO_API_PATH }}'
